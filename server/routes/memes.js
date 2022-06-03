7046fe83b054f60230024786c807c3d6e6544126
FlimothyCrow <timshogren@gmail.com>
2022-06-03 11:49:12 -0600

    defined /meme/ and /upvote/

--- server/routes/memes.js
+++ server/routes/memes.js
@@ -2,16 +2,43 @@ const express = require("express")
 const router = express.Router()
 const memes = require("../services/memes")
 
-/* GET memes. */
-router.get("/", async function (req, res, next) {
+/* 
+  Router for /memes/ 
+*/
+
+async function wrapErrors(fn) {
     try {
-        // await automatically colors the function and upper functions as async
-        res.json(await memes.getMultiple(req.query.page)) // response exists and we append to it
-        // we NEED await/async anytime we go outside the language runtime (database, filesystem, other servers)
+        await fn()
     } catch (err) {
         console.error(`Error while getting memes `, err.message)
         next(err)
     }
+}
+
+// GET /memes/
+router.get("/", async function (req, res, next) {
+    await wrapErrors(async () => {
+        // build through lambda so that the query executed WITHIN the try on line 10
+        // await automatically colors the function and upper functions as async
+        res.json(await memes.getMultiple()) // response exists and we append to it
+        // we NEED await/async anytime we go outside the language runtime (database, filesystem, other servers)
+    })
+})
+
+// GET /memes/meme/1
+router.get("/meme/:memeId", async function (req, res, next) {
+    await wrapErrors(async () => {
+        res.json(await memes.getMeme(req.params.memeId));
+    })
+})
+
+// GET /memes/upvote/:memeId
+router.get("/upvote/:memeId", async function (req, res, next) {
+    await wrapErrors(async () => {
+        let memeId = req.params.memeId; 
+        await memes.upvoteMeme(memeId);
+        res.json(await memes.getMeme(memeId));
+    })
 })
 
 module.exports = router
--- server/services/memes.js
+++ server/services/memes.js
@@ -2,18 +2,22 @@ const db = require("./db")
 const helper = require("../helper")
 const config = require("../config")
 
-async function getMultiple(page = 1) {
-    const offset = helper.getOffset(page, config.listPerPage)
-    const rows = await db.query(`SELECT * FROM memes LIMIT ${offset},${config.listPerPage}`)
-    const data = helper.emptyOrRows(rows)
-    const meta = { page }
+async function upvoteMeme(memeId) {
+    await db.query(`UPDATE memes SET upvotes = upvotes + 1 WHERE id = ${memeId};`)
+}
 
-    return {
-        data,
-        meta,
-    }
+async function getMeme(memeId) {
+    const rows = await db.query(`SELECT * FROM memes WHERE id = ${memeId};`)
+    return rows.length === 1 ? rows[0] : {} // query returns an array
+}
+
+async function getMultiple() {
+    const rows = await db.query(`SELECT * FROM memes;`)
+    return helper.emptyOrRows(rows)
 }
 
 module.exports = {
     getMultiple,
+    getMeme,
+    upvoteMeme
 }
