5cd995957a15dd78653f0a87f8218e8b454e3599
FlimothyCrow <timshogren@gmail.com>
2022-06-11 12:56:34 -0600

    updated routes and added upvote button

--- client/src/App.js
+++ client/src/App.js
@@ -1,32 +1,29 @@
-import React from "react"
+import React, { useEffect } from "react"
 
 function App() {
-    const [word, setWord] = React.useState("password")
-    const [associations, setAssociations] = React.useState(null)
-    const getAssociations = () => {
-        fetch("/api/associations/" + word)
+    const [meme, setMeme] = React.useState({}) // setMeme sister function triggers re-render
+    const updoot = (memeID) => {
+        fetch("http://localhost:3001/memes/upvote/" + memeID) // fetch pings outwards to API
             .then((result) => result.json())
-            .then((body) => setAssociations(body))
+            .then((body) => setMeme(body))
     }
-    console.log(associations)
-    console.log(word)
+    useEffect(() => {
+        fetch("http://localhost:3001/memes/meme/" + "1") // fetch pings outwards to API
+            .then((result) => result.json())
+            .then((body) => setMeme(body))
+    }, [])
+
+    console.log(meme)
     return (
         <div className="app">
-            <h1>Word Associations Map</h1>
-            <input value={word} onChange={(e) => setWord(e.target.value)} />
-            <button onClick={getAssociations}>Find Associations</button>
-            {associations &&
-                (Object.keys(associations).length === 0 ? (
-                    <p>No results</p>
-                ) : (
-                    <div>
-                        {Object.entries(associations).map(([association, score], idx) => (
-                            <span key={idx} style={{ fontSize: Math.pow(score * 500, 2) / 200 }}>
-                                {association}{" "}
-                            </span>
-                        ))}
-                    </div>
-                ))}
+            <h1>filename is equal to {meme.filename}</h1>
+            <h2>the title of the meme is {meme.title}</h2>
+            <h3>thank you to op {meme.user_id}</h3>
+            <div>has {meme.upvotes} many updoots</div>{" "}
+            <button type="button" onClick={() => updoot(meme.id)}>
+                Click for updoot
+            </button>
+            <img src={"http://localhost:3001/memes/img/" + meme.id} alt="hello"></img>
         </div>
     )
 }
